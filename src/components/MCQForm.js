135f51a3b23da617ac05b22e2d9c0ba2bcb45ff6
Ankit Tanwar <tankitofficial01@gmail.com>
2023-10-23 23:49:22 +0530

    update

--- src/components/MCQForm.js
+++ src/components/MCQForm.js
@@ -11,18 +11,47 @@ const answerToNumber = {
 };
 
 export default function MCQForm() {
-  
   const searchParams = useSearchParams();
   const search = searchParams.get('qtest');
-
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [correctAnswers, setCorrectAnswers] = useState([]);
   const [score, setScore] = useState(0);
+  const [expired, setExpired] = useState(false); // Track if time is expired
+  
+
+  const [submitted, setSubmitted] = useState(false);
 
   useEffect(() => {
     fetchQuestions();
+    var countDownDate = new Date();
+    countDownDate.setMinutes(countDownDate.getMinutes() + 1);
+
+    const x = setInterval(function () {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var minutes = Math.floor(distance / 60000);
+      var seconds = Math.floor((distance % 60000) / 1000);
+      var formattedTime = minutes + "m " + seconds + "s";
+
+      // Ensure that the element exists in the DOM before updating its innerHTML
+      const countdownElement = document.getElementById("countdown");
+      if (countdownElement) {
+        countdownElement.innerHTML = formattedTime;
+      }
+
+      if (distance <= 0) {
+        clearInterval(x);
+        // Make sure to check the element again before updating its innerHTML
+        if (countdownElement) {
+          countdownElement.innerHTML = "EXPIRED";
+        }
+        setExpired(true); // Time is expired
+      }
+    }, 1000);
+
+    
   }, []);
 
   function fetchQuestions() {
@@ -67,16 +96,19 @@ export default function MCQForm() {
       return totalScore;
     }, 0);
   };
-
+  
   const submitAnswer = () => {
     // Calculate the score
     const newScore = calculateScore();
-  
+
     // Move to the next question
     setCurrentQuestionIndex(currentQuestionIndex + 1);
-  
+
     // Update the score state
     setScore(newScore);
+
+    // Mark the quiz as submitted
+    setSubmitted(true);
   };
   
 
@@ -94,50 +126,100 @@ export default function MCQForm() {
 
   return (
     <div className="container">
-      {currentQuestionIndex < questions.length && (
-        <div className="mb-4">
-          <h4 className='d-flex justify-content-between'>{questions[currentQuestionIndex].name} <span>{currentQuestionIndex+1}/{questions.length}</span></h4>
-          <ul className="list-group">
-            {questions[currentQuestionIndex].options.map((option, optionIndex) => (
-              <li key={optionIndex} className="list-group-item">
-                <label className="form-check-label">
-                  <input
-                    type="radio"
-                    name={`question${currentQuestionIndex}`}
-                    value={optionIndex}
-                    checked={userAnswers[currentQuestionIndex] === optionIndex}
-                    onChange={() => handleAnswerSelect(optionIndex)}
-                    className="form-check-input me-3"
-                  />
-                  {option}
-                </label>
-              </li>
-            ))}
-          </ul>
-          <div className="mt-2 d-flex justify-content-between">
-            {currentQuestionIndex > 0 && (
-              <button onClick={previousQuestion} className="btn btn-secondary">
-                Previous
-              </button>
-            )}
-            {currentQuestionIndex < questions.length - 1 && (
-              <button onClick={nextQuestion} className="btn btn-primary">
-                Next
-              </button>
-            )}
-            {currentQuestionIndex === questions.length - 1 && (
-              <button onClick={submitAnswer} className="btn btn-primary">
-                Submit
-              </button>
-            )}
-          </div>
-        </div>
-      )}
-
-      {currentQuestionIndex === questions.length && (
+      {submitted ? ( // If the quiz is submitted, don't display countdown
         <div>
           <h3>Quiz Complete!</h3>
           <p>Your Score: {score} out of {questions.length}</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Question</th>
+                <th scope="col">Your Answer</th>
+                <th scope="col">The Correct Answer</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {questions &&
+                questions.map((cv, idx) => {
+                  return (
+                    <tr key={idx}>
+                      <td>{cv.name}</td>
+                      <td>
+                        {userAnswers[idx] === null
+                          ? 'Not answered'
+                          : cv.options[userAnswers[idx]]}
+                      </td>
+                      <td>{cv.options[cv.correctAnswer]}</td>
+                      <td>
+                        {userAnswers[idx] === cv.correctAnswer
+                          ? 'Correct'
+                          : 'Incorrect'}
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div>
+          <div id="countdown"></div>
+          {expired ? ( // If time is expired, show "EXPIRED" and score card
+            <div>
+              <h3>Time's Up! Quiz Expired</h3>
+              <p>Your Score: {score} out of {questions.length}</p>
+            </div>
+          ) : currentQuestionIndex < questions.length ? ( // If time is not expired, handle questions
+            <div className="mb-4">
+              <h4 className="d-flex justify-content-between">
+                {questions[currentQuestionIndex].name}{' '}
+                <span>
+                  {currentQuestionIndex + 1}/{questions.length}
+                </span>
+              </h4>
+              <ul className="list-group">
+                {questions[currentQuestionIndex].options.map((option, optionIndex) => (
+                  <li key={optionIndex} className="list-group-item">
+                    <label className="form-check-label">
+                      <input
+                        type="radio"
+                        name={`question${currentQuestionIndex}`}
+                        value={optionIndex}
+                        checked={userAnswers[currentQuestionIndex] === optionIndex}
+                        onChange={() => handleAnswerSelect(optionIndex)}
+                        className="form-check-input me-3"
+                      />
+                      {option}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-2 d-flex justify-content-between">
+                {currentQuestionIndex > 0 && (
+                  <button onClick={previousQuestion} className="btn btn-secondary">
+                    Previous
+                  </button>
+                )}
+                {currentQuestionIndex < questions.length - 1 && (
+                  <button onClick={nextQuestion} className="btn btn-primary">
+                    Next
+                  </button>
+                )}
+                {currentQuestionIndex === questions.length - 1 && (
+                  <button onClick={submitAnswer} className="btn btn-primary">
+                    Submit
+                  </button>
+                )}
+              </div>
+            </div>
+          ) : (
+            // If all questions are answered, show the score card
+            <div>
+              <h3>Quiz Complete!</h3>
+              <p>Your Score: {score} out of {questions.length}</p>
+            </div>
+          )}
         </div>
       )}
     </div>
